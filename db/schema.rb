f4d2500ee48e40d3f411de12f3342e352648aa1b
Christian Perez <perezchristian1012@gmail.com>
2020-01-17 18:29:17 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200117182722) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "preferences", force: :cascade do |t|
+    t.boolean "allow_create_artists"
+    t.boolean "allow_create_songs"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
 
 end
